e42a2c7713c54609d986b1cba4ee9054c74a9052
toddkblake <todd.k.blake@gmail.com>
2017-02-24 12:10:33 -0500

    Remove React minification warning

---
+++ webpack.config.prod.js
@@ -0,0 +1,38 @@
+var path = require('path');
+var webpack = require("webpack");
+
+module.exports = {
+  entry: './frontend/dropify.jsx',
+  output: {
+    path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+    filename: 'bundle.js',
+  },
+  plugins:[
+    new webpack.DefinePlugin({
+      'process.env':{
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress:{
+        warnings: true
+      }
+    })
+  ],
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/],
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.jsx', '*']
+  }
+};
